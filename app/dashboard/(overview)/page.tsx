f3a216d5fa3f7b4546bd42e9a65334c511167128
Maxim Afteniy <maxim.afteniy@gmail.com>
2024-10-27 07:19:48 +0100

    Added latest changes, the page is complete now

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,12 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, InvoiceSkeleton, CardSkeleton } from '@/app/ui/skeletons';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Dashboard',
+};
+
 export default async function Page() {
     return (
         <main>
